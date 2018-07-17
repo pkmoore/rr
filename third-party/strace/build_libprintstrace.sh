d6fa1bc624f0132a281ada127505371aa3082f98
Joey Pabalinas <joeypabalinas@gmail.com>
2018-07-16 20:21:40 -1000

    bootstrap: move ./configure step to bootstrap script
    Signed-off-by: Joey Pabalinas <joeypabalinas@gmail.com>

--- third-party/strace/build_libprintstrace.sh
+++ third-party/strace/build_libprintstrace.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# ./bootstrap
-# This always fails at the end but we need the objects in place for the next
-# make
 make clean
 make -f Makefile.libstrace clean
 make
--- third-party/strace/build_libstrace.sh
+++ third-party/strace/build_libstrace.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-unset CFLAGS CPATH CPPFLAGS CXXFLAGS LDFLAGS
 ./bootstrap
-./configure --without-libunwind
-make clean
-make -f Makefile.libstrace clean
 make
 make -f Makefile.libstrace
